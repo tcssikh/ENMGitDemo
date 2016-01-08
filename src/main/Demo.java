08510f8ee729db6502f51a364e9a53039d058d0f
Siva Kumar <kumar.hsiva@tcs.com>
2016-01-08 10:24:01 +0530

    Modification in classic branch

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -7,7 +7,7 @@ public class Demo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome");
+		System.out.println("Welcome to Classic");
 	}
 
 }
